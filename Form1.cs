62239ef68250757f6966cf99e0fc135be48642b9
Derjen Frick <ST10371246@vgconnect.edu.za>
2024-04-12 12:37:22 +0200

    removed code

--- Form1.cs
+++ Form1.cs
@@ -30,27 +30,18 @@ namespace WindowsFormsApp3
         private void btnSave1_Click(object sender, EventArgs e)
 
         {
-            if (txtPlayer1Name.Text != "" && txtDragon1Name.Text != "")
-            {
-                player1Name = txtPlayer1Name.Text;
-                dragon1Name = txtDragon1Name.Text;
-                rtbLog.Text = "Player name:" + player1Name + "\n Dragon Name:" + dragon1Name + "\nplayer choice" + player1Choice;
-            }
-            else
-            {
-                rtbLog.Text = " you didnt enter names dimwit";
-            }
+          
             
         }
 
         private void radFire1_CheckedChanged(object sender, EventArgs e)
         {
-            player1Choice = 1;
+           
         }
 
         private void radIce1_CheckedChanged(object sender, EventArgs e)
         {
-            player1Choice = 2;
+            
         }
 
         private void gbx2_Enter(object sender, EventArgs e)
@@ -60,34 +51,23 @@ namespace WindowsFormsApp3
 
         private void radWind1_CheckedChanged(object sender, EventArgs e)
         {
-            player1Choice = 3;
+           
         }
 
         private void radEarth1_CheckedChanged(object sender, EventArgs e)
         {
-            player1Choice = 4;
+            
         }
 
         private void btnStart_Click(object sender, EventArgs e)
 
         {
-            var Form2 = new Form2();
-            this.Hide();
-            Form2.ShowDialog();
+
         }
 
         private void btnSave2_Click(object sender, EventArgs e)
         {
-            if ( txtPlayer2Name.Text != "" && txtDragon2Name.Text != "")
-            {
-                player2Name = txtPlayer2Name.Text;
-                dragon2Name = txtDragon2Name.Text;
-                rtbLog2.Text = "Player name:" + player2Name + "\n Dragon Name:" + dragon2Name + "\nplayer choice";
-            }
-            else
-            {
-                rtbLog2.Text = " you didnt enter names dimwit";
-            }
+           
         }
     }
 }
